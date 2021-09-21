398af3d0427b300ecf8bba334fdceac1ef700805
hamzausmani302 <hamzausmani021@gmail.com>
2021-09-21 14:07:41 +0500

    a

--- src/Pages/Adminstration.js
+++ src/Pages/Adminstration.js
@@ -1,27 +1,27 @@
 import React , {useState} from 'react'
 import {Container , Row , Col, Button} from 'react-bootstrap';
-
+import '../assets/css/style.css';
 
 export default function Adminstration() {
     const [open,setopen] = useState(true);
     return (
         <Container fluid> 
             <Row>
-            <Button className="btn btn-primary w-25" onClick={()=>{setopen(!open);}}>CLICK ME</Button>
+            <Button className="btn btn-primary w-25" onClick={()=>{setopen(!open);}}></Button>
             
             </Row>
             <Row>  
-                {open ? <Col className="animate col-lg-2 ">
+                {open ? <Col style={{animationDelay:5000}} className="animate col-lg-2 col-sm-12 ">
                         <h1>MENU</h1>
                         <ul>
                             <li>Products</li>
                             <li>Categories</li>
-                            <li>Orders</li>
+                            <li>Orders in line</li>
                         </ul>
                         
 
                 </Col> : null  } 
-                {!open ? <Col className="col-lg-12 bg-secondary">hello</Col> :<Col className="col-lg-10 bg-secondary">hello</Col> }
+                {!open ? <Col className="col-lg-12 col-sm-12  bg-secondary">hello</Col> :<Col className="col-lg-10 bg-secondary">hello</Col> }
                 
             </Row>
         </Container>
